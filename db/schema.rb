e8d47a95db92c9e3426115212cac45b32a3727a6
Yuval Drori <yuvald29@protonmail.com>
2019-09-15 17:57:42 +0300

    add preferences model, add routes and user controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_15_140316) do
+ActiveRecord::Schema.define(version: 2019_09_15_144809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 2019_09_15_140316) do
     t.index ["category_id"], name: "index_places_on_category_id"
   end
 
+  create_table "preferences", force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_preferences_on_category_id"
+    t.index ["user_id"], name: "index_preferences_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -63,4 +72,6 @@ ActiveRecord::Schema.define(version: 2019_09_15_140316) do
   add_foreign_key "bookmarks", "places"
   add_foreign_key "bookmarks", "users"
   add_foreign_key "places", "categories"
+  add_foreign_key "preferences", "categories"
+  add_foreign_key "preferences", "users"
 end
