272726d601aaacc58156c7acf634b764b11d0371
Yuval Drori <yuvald29@protonmail.com>
2019-09-15 18:05:10 +0300

    add bookmarks controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'bookmarks/index'
+  get 'bookmarks/show'
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/update'
+  get 'bookmarks/edit'
+  get 'bookmarks/delete'
   devise_for :users
   root to: 'pages#home'
   resources :user, only: [:show]
