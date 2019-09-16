a910c646610bd5d05eb2c5dbfc60a08a8aca4293
Yuval Drori <yuvald29@protonmail.com>
2019-09-16 16:59:40 +0300

    create a nokogiri scraper to get a list of categories that we can use to get actual places from the yelp api, turn it into a service and seed the database with it (as category objects)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :places
+  resources :categories
   resources :user, only: [:show] do
     resources :bookmarks
     resources :preferences
