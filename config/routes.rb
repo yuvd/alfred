4cd5264e34ba483a538d5fd84aad5728e60eafa5
Janina Peterek <jpeterek@google.com>
2019-09-17 10:59:01 +0300

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   get 'places/businesses'
   devise_for :users
   root to: 'pages#home'
-  
+   get 'places/map', to: 'places#map', as: 'map'
+
   resources :places
   resources :categories
   resources :user, only: [:show] do
     resources :bookmarks
     resources :preferences
   end
-  
-  get 'places/map', to: 'places#map', as: 'map'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
