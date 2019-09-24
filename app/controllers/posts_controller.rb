40460298598c093cd154ae7d527a56a55654ff1f
Janina Peterek <jpeterek@google.com>
2019-09-24 14:22:03 +0300

    edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @forum = @post.forum
   end
 end
