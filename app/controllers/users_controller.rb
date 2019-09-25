c2b92e492a5313ce06cd549200a09b7a5e370fac
Yuval Drori <yuvald29@protonmail.com>
2019-09-25 17:16:56 +0300

    add places to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.update(user_params)
     if params[:from] == 'landing'
-      redirect_to places_path
+      redirect_to places_path(category: 'all')
     else
       redirect_to profile_path
     end
